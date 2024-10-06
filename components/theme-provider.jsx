6ee8c43daa61a572afa3e5d691133da4d2221342
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-07 01:16:07 +0500

    update

--- components/theme-provider.jsx
+++ components/theme-provider.jsx
@@ -4,6 +4,6 @@ import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 
-export function ThemeProvider({ children, ...props }:) {
+export function ThemeProvider({ children, ...props }) {
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
